6fab6cb918516853ea8081acf542a8620b494db1
Guilherme Campelo <campelo.guilherme@hotmail.com>
2024-09-22 17:08:11 -0300

    api

--- js/dom.js
+++ js/dom.js
@@ -104,7 +104,7 @@ document.getElementById('gerarPergunta').addEventListener('click', async functio
     mostrarCarregando(); // Mostra a mensagem de carregamento
 
     try {
-        const response = await fetch('https://cesio.vercel.app/apigerarPergunta', {
+        const response = await fetch('https://cesio.vercel.app/api/gerarPergunta', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
